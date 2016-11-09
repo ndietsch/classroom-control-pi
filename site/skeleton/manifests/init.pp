f3d21ea9dd1fc62d7d07facfc8b8f02b3835acad
Nathan Dietsch <njd@ndietsch.com>
2016-11-09 16:58:18 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,9 +1,16 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => directory,   # what value should go here?
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+  }
+  
+  file { '/etc/shel/.bashrc':
+    ensure => file,
+    source => 'puppet:///skeleton/bashrc',
+    mode => '0755',
+    requires => File['/etc/skel'],
   }
   
   # add a resource to manage /etc/skel/.bashrc
